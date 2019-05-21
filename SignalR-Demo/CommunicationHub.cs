1bd9da1d8c125cb73e06724ad3aa62cccb86c53d
Nidhi Srivastawa (Prowareness) <n.srivastawa@prowareness.nl>
2019-05-21 16:57:36 +0200

    adding project files

---
+++ SignalR-Demo/CommunicationHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_Demo
+{
+    public class CommunicationHub :Hub
+{
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "grp1");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "grp1");
+        }
+
+        public async Task BroadcastMessage(string msg)
+        {
+            await Clients.OthersInGroup("grp1").SendAsync("RecieveMessage", msg);
+
+        }
+    }
+}
---
+++ SignalR-Demo/wwwroot/js/texteditor.js
@@ -0,0 +1,17 @@
+﻿
+var editor = document.getElementById("editor");
+
+const connection = new signalR.HubConnectionBuilder().withUrl("/communicationhub").build();
+connection.start().catch(err => console.error(err));
+
+connection.on("RecieveMessage", (text) => {
+    editor.value = text;
+    editor.focus();
+    editor.setSelectionRange(editor.value.length, editor.value.length);
+}); 
+
+function change() {
+    connection.invoke("BroadcastMessage", editor.value).catch(err => console.error(err));
+}
+
+
